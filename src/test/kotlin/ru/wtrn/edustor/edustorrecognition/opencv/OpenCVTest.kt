9a9beb8035155633867d6a9d8296440c01d57f52
Dmitry Romanov <me@wtrn.ru>
2021-03-19 12:42:16 +0300

    WIP: Experiments with OpenCV

--- src/test/kotlin/ru/wtrn/edustor/edustorrecognition/opencv/OpenCVTest.kt
+++ src/test/kotlin/ru/wtrn/edustor/edustorrecognition/opencv/OpenCVTest.kt
@@ -28,32 +28,27 @@ class OpenCVTest {
         Imgproc.dilate(mat, mat, dilateElement);
 
         val lines = Mat()
-        Imgproc.HoughLines(mat, lines, 1.0, Math.PI / 180, 200, 0.0, 0.0, 0.0, Math.PI)
+        val thetaModifier = Math.PI / 180 * 80
+        Imgproc.HoughLines(mat, lines, 1.0, Math.PI / 180, 1350, 0.0, 0.0, 0.0 + thetaModifier, thetaModifier + Math.PI / 180 * 20)
+
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_GRAY2RGB)
 
         val rows = lines.rows()
-//        (0 until rows).forEach { i ->
-//            val data = lines.get(i, 0)
-//            val rho = data[0]
-//            val theta = data[1]
-//            val cosTheta = Math.cos(theta)
-//            val sinTheta = Math.sin(theta)
-//            val x0 = cosTheta * rho
-//            val y0 = sinTheta * rho
-//
-//            val pt1 = Point(x0 + 10000 * (-sinTheta), y0 + 10000 * cosTheta)
-//            val pt2 = Point(x0 - 10000 * (-sinTheta), y0 - 10000 * cosTheta)
-//
-//            Imgproc.line(srcMat, pt1, pt2, Scalar(0.0, 0.0, 255.0), 2)
-//        }
+        (0 until rows).forEach { i ->
+            val data = lines.get(i, 0)
+            val rho = data[0]
+            val theta = data[1]
+            val cosTheta = Math.cos(theta)
+            val sinTheta = Math.sin(theta)
+            val x0 = cosTheta * rho
+            val y0 = sinTheta * rho
 
-//        if (hierarchy.size().height > 0 && hierarchy.size().width > 0) {
-//             for each contour, display it in blue
-//            var idx = 0
-//            while (idx >= 0) {
-//                Imgproc.drawContours(srcMat, contours, idx, Scalar(250.0, 0.0, 0.0))
-//                idx = hierarchy[0, idx][0].toInt()
-//            }
-//        }
+            val pt1 = Point(x0 + 10000 * (-sinTheta), y0 + 10000 * cosTheta)
+            val pt2 = Point(x0 - 10000 * (-sinTheta), y0 - 10000 * cosTheta)
+
+            Imgproc.line(mat, pt1, pt2, Scalar(0.0, 0.0, 255.0), 2)
+            Imgproc.line(srcMat, pt1, pt2, Scalar(0.0, 0.0, 255.0), 2)
+        }
 
         File(outDirectory, "result.png").writeBytes(srcMat.toPng())
         File(outDirectory, "mat.png").writeBytes(mat.toPng())
