e9266dac5b9657e9e022cdd23473dde77965f766
Dmitry Romanov <me@wtrn.ru>
2021-03-19 16:52:05 +0300

    WIP: Experiments with OpenCV

--- src/test/kotlin/ru/wtrn/edustor/edustorrecognition/opencv/OpenCVQRDetectionTest.kt
+++ src/test/kotlin/ru/wtrn/edustor/edustorrecognition/opencv/OpenCVQRDetectionTest.kt
@@ -19,7 +19,7 @@ class OpenCVQRDetectionTest {
     fun testOpenCV() {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
 
-        val srcMat = javaClass.getResource("/test_qr_marker.jpeg").readBytes().toImageMat()
+        val srcMat = javaClass.getResource("/test_page.png").readBytes().toImageMat()
         val mat = Mat()
         Imgproc.cvtColor(srcMat, mat, Imgproc.COLOR_RGB2GRAY)
         Imgproc.blur(mat, mat, Size(3.0, 3.0))
@@ -43,6 +43,7 @@ class OpenCVQRDetectionTest {
         val hierarchy = Mat()
         Imgproc.findContours(mat, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
 
+        val parentsCache = HashMap<Int, Int>()
         Imgproc.cvtColor(mat, mat, Imgproc.COLOR_GRAY2RGB)
         contours.forEachIndexed { i, contour ->
             val meta = hierarchy[0, i]
@@ -51,11 +52,30 @@ class OpenCVQRDetectionTest {
                 true -> Scalar(0.0, 255.0, 0.0)
                 false -> Scalar(0.0, 0.0, 255.0)
             }
-            val firstPoint = contour[5, 0]
-            Imgproc.drawContours(mat, contours, i, color, 1)
+            if (calculateParentsCount(contourIndex = i, hierarchy = hierarchy, parentsCache = parentsCache) >= 5) {
+                Imgproc.drawContours(mat, contours, i, color, 1)
+                Imgproc.drawContours(srcMat, contours, i, color, 1)
+            }
         }
 
         File(outDirectory, "result.png").writeBytes(srcMat.toPng())
         File(outDirectory, "mat.png").writeBytes(mat.toPng())
+    }
+
+    fun calculateParentsCount(contourIndex: Int, hierarchy: Mat, parentsCache: HashMap<Int, Int>): Int {
+        parentsCache[contourIndex]?.let {
+            return it
+        }
+
+        val hierarchyMeta = hierarchy[0, contourIndex]
+        val parentIndex = hierarchyMeta[3]
+        if (parentIndex < 0) {
+            parentsCache[contourIndex] = 0
+            return 0
+        }
+
+        val parentsCount = calculateParentsCount(parentIndex.toInt(), hierarchy, parentsCache) + 1
+        parentsCache[contourIndex] = parentsCount
+        return parentsCount
     }
 }
