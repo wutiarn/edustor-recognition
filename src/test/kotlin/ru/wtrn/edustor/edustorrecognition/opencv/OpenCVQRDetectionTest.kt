ef7bc1fa30754220f0bf238b6b10d5837e934eb3
Dmitry Romanov <me@wtrn.ru>
2021-03-19 16:21:30 +0300

    WIP: Experiments with OpenCV

--- src/test/kotlin/ru/wtrn/edustor/edustorrecognition/opencv/OpenCVQRDetectionTest.kt
+++ src/test/kotlin/ru/wtrn/edustor/edustorrecognition/opencv/OpenCVQRDetectionTest.kt
@@ -19,12 +19,13 @@ class OpenCVQRDetectionTest {
     fun testOpenCV() {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
 
-        val srcMat = javaClass.getResource("/test_page.png").readBytes().toImageMat()
+        val srcMat = javaClass.getResource("/test_qr_marker.jpeg").readBytes().toImageMat()
         val mat = Mat()
         Imgproc.cvtColor(srcMat, mat, Imgproc.COLOR_RGB2GRAY)
         Imgproc.blur(mat, mat, Size(3.0, 3.0))
         Core.bitwise_not(mat, mat)
         Imgproc.threshold(mat, mat, 100.0, 255.0, Imgproc.THRESH_BINARY)
+//        Core.bitwise_not(mat, mat)
         Imgproc.Canny(mat, mat, 5.0, 30.0, 3, false)
 
 //        Imgproc.blur(mat, mat, Size(3.0, 3.0))
@@ -40,18 +41,19 @@ class OpenCVQRDetectionTest {
 
         val contours: List<MatOfPoint> = ArrayList()
         val hierarchy = Mat()
-        Imgproc.findContours(mat, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+        Imgproc.findContours(mat, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
 
         Imgproc.cvtColor(mat, mat, Imgproc.COLOR_GRAY2RGB)
-        if (hierarchy.size().height > 0 && hierarchy.size().width > 0) {
-//             for each contour, display it in blue
-            var idx = 0
-            while (idx >= 0) {
-                Imgproc.drawContours(mat, contours, idx, Scalar(0.0, 0.0, 255.0))
-                idx = hierarchy[0, idx][0].toInt()
+        contours.forEachIndexed { i, contour ->
+            val meta = hierarchy[0, i]
+            val parent = meta[3]
+            val color = when (parent > 0) {
+                true -> Scalar(0.0, 255.0, 0.0)
+                false -> Scalar(0.0, 0.0, 255.0)
             }
+            val firstPoint = contour[5, 0]
+            Imgproc.drawContours(mat, contours, i, color, 1)
         }
-
 
         File(outDirectory, "result.png").writeBytes(srcMat.toPng())
         File(outDirectory, "mat.png").writeBytes(mat.toPng())
