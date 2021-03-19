f4b309429a95902da64ffd60718176c971d93d8c
Dmitry Romanov <me@wtrn.ru>
2021-03-19 12:20:23 +0300

    WIP: Experiments with OpenCV

--- src/test/kotlin/ru/wtrn/edustor/edustorrecognition/opencv/OpenCVTest.kt
+++ src/test/kotlin/ru/wtrn/edustor/edustorrecognition/opencv/OpenCVTest.kt
@@ -1,20 +1,11 @@
 package ru.wtrn.edustor.edustorrecognition.opencv
 
 import org.junit.jupiter.api.Test
-import org.opencv.core.Core
-import org.opencv.core.Mat
-import org.opencv.core.Size
+import org.opencv.core.*
 import org.opencv.imgproc.Imgproc
 import ru.wtrn.edustor.edustorrecognition.util.toImageMat
 import ru.wtrn.edustor.edustorrecognition.util.toPng
 import java.io.File
-import org.opencv.core.MatOfPoint
-import org.opencv.core.Scalar
-
-
-
-
-
 
 
 class OpenCVTest {
@@ -36,20 +27,33 @@ class OpenCVTest {
         val dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, Size(5.0, 5.0))
         Imgproc.dilate(mat, mat, dilateElement);
 
-        val contours: List<MatOfPoint> = ArrayList()
-        val hierarchy = Mat()
-        Imgproc.findContours(mat, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+        val lines = Mat()
+        Imgproc.HoughLines(mat, lines, 1.0, Math.PI / 180, 200, 0.0, 0.0, 0.0, Math.PI)
 
-        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_GRAY2RGB)
+        val rows = lines.rows()
+//        (0 until rows).forEach { i ->
+//            val data = lines.get(i, 0)
+//            val rho = data[0]
+//            val theta = data[1]
+//            val cosTheta = Math.cos(theta)
+//            val sinTheta = Math.sin(theta)
+//            val x0 = cosTheta * rho
+//            val y0 = sinTheta * rho
+//
+//            val pt1 = Point(x0 + 10000 * (-sinTheta), y0 + 10000 * cosTheta)
+//            val pt2 = Point(x0 - 10000 * (-sinTheta), y0 - 10000 * cosTheta)
+//
+//            Imgproc.line(srcMat, pt1, pt2, Scalar(0.0, 0.0, 255.0), 2)
+//        }
 
-        if (hierarchy.size().height > 0 && hierarchy.size().width > 0) {
-            // for each contour, display it in blue
-            var idx = 0
-            while (idx >= 0) {
-                Imgproc.drawContours(srcMat, contours, idx, Scalar(250.0, 0.0, 0.0))
-                idx = hierarchy[0, idx][0].toInt()
-            }
-        }
+//        if (hierarchy.size().height > 0 && hierarchy.size().width > 0) {
+//             for each contour, display it in blue
+//            var idx = 0
+//            while (idx >= 0) {
+//                Imgproc.drawContours(srcMat, contours, idx, Scalar(250.0, 0.0, 0.0))
+//                idx = hierarchy[0, idx][0].toInt()
+//            }
+//        }
 
         File(outDirectory, "result.png").writeBytes(srcMat.toPng())
         File(outDirectory, "mat.png").writeBytes(mat.toPng())
