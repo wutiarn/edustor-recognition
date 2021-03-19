d6e098b4d497182495e43964e12921b2caff7dcf
Dmitry Romanov <me@wtrn.ru>
2021-03-19 11:45:11 +0300

    WIP: Experiments with OpenCV

--- src/test/kotlin/ru/wtrn/edustor/edustorrecognition/opencv/OpenCVTest.kt
+++ src/test/kotlin/ru/wtrn/edustor/edustorrecognition/opencv/OpenCVTest.kt
@@ -8,6 +8,14 @@ import org.opencv.imgproc.Imgproc
 import ru.wtrn.edustor.edustorrecognition.util.toImageMat
 import ru.wtrn.edustor.edustorrecognition.util.toPng
 import java.io.File
+import org.opencv.core.MatOfPoint
+import org.opencv.core.Scalar
+
+
+
+
+
+
 
 class OpenCVTest {
 
@@ -28,7 +36,22 @@ class OpenCVTest {
         val dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, Size(5.0, 5.0))
         Imgproc.dilate(mat, mat, dilateElement);
 
-        val resultBytes = mat.toPng()
-        val outFile = File(outDirectory, "opencv.png").writeBytes(resultBytes)
+        val contours: List<MatOfPoint> = ArrayList()
+        val hierarchy = Mat()
+        Imgproc.findContours(mat, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_GRAY2RGB)
+
+        if (hierarchy.size().height > 0 && hierarchy.size().width > 0) {
+            // for each contour, display it in blue
+            var idx = 0
+            while (idx >= 0) {
+                Imgproc.drawContours(srcMat, contours, idx, Scalar(250.0, 0.0, 0.0))
+                idx = hierarchy[0, idx][0].toInt()
+            }
+        }
+
+        File(outDirectory, "result.png").writeBytes(srcMat.toPng())
+        File(outDirectory, "mat.png").writeBytes(mat.toPng())
     }
 }
