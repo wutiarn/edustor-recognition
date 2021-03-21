fce2213299576785391cadabde0233790f190a82
Dmitry Romanov <me@wtrn.ru>
2021-03-21 13:04:41 +0300

    WIP: QrRotationAngleCalculator

--- src/main/kotlin/ru/wtrn/edustor/edustorrecognition/util/qr/QrRotationAngleCalculator.kt
+++ src/main/kotlin/ru/wtrn/edustor/edustorrecognition/util/qr/QrRotationAngleCalculator.kt
@@ -3,6 +3,8 @@ package ru.wtrn.edustor.edustorrecognition.util.qr
 import org.opencv.core.Point
 import java.lang.IllegalStateException
 import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 object QrRotationAngleCalculator {
     fun calculateQrCodeAngle(qrMarkersLocation: List<Point>): Double {
@@ -20,9 +22,11 @@ object QrRotationAngleCalculator {
         val p2 = other[1]
 
         val slope = calculateSlope(p1, p2)
+        val perpendicularDistance = calculatePerpendicularDistance(topLeftPoint, p1, p2)
         val rotationRad = Math.atan(slope) - Math.PI / 4
 
-        return rotationRad * 180 / Math.PI // Convert rad to degrees
+        val rotationDegrees = rotationRad * 180 / Math.PI
+        return rotationDegrees // Convert rad to degrees
     }
 
     private fun isTopLeft(point: Point, other: List<Point>): Boolean {
@@ -30,12 +34,18 @@ object QrRotationAngleCalculator {
         val d1 = distances[0]
         val d2 = distances[1]
 
-        val delta = d1 - d2
+        val delta = abs(d1 - d2)
         val maxDelta = d1 * 0.01
         return delta < maxDelta
     }
 
     private fun calculateSlope(p1: Point, p2: Point): Double {
-        return abs(p1.y - p2.y) / abs(p1.x - p2.x)
+        return p1.y - p2.y / p1.x - p2.x
+    }
+
+    private fun calculatePerpendicularDistance(point: Point, lineP1: Point, lineP2: Point): Double {
+        // See https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Line_defined_by_two_points
+        return abs((lineP2.x - lineP1.x) * (lineP1.y - point.y) - (lineP1.x - point.x) * (lineP2.y - lineP1.y)) /
+                sqrt((lineP2.x - lineP1.x).pow(2) + (lineP2.y - lineP1.y).pow(2))
     }
 }
