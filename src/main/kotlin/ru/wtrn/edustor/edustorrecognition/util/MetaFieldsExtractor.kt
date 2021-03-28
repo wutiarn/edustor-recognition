de749c628a4fea88815e711c9ac9d8d9ab037902
Dmitry Romanov <me@wtrn.ru>
2021-03-28 23:05:47 +0300

    Update MetaFieldsExtractor docs

--- src/main/kotlin/ru/wtrn/edustor/edustorrecognition/util/MetaFieldsExtractor.kt
+++ src/main/kotlin/ru/wtrn/edustor/edustorrecognition/util/MetaFieldsExtractor.kt
@@ -10,8 +10,17 @@ object MetaFieldsExtractor {
     }
 
     fun getArea(qrAndMetaArea: Rect): Rect {
+        /**
+         * 0.4 is size of marker side, 0.5 - margin between marker and grid.
+         * Both of this values are fractions of single cell side.
+         */
         val markerCellHeigth = 0.4 + 0.25
         val cellSide = qrAndMetaArea.height / (56 + markerCellHeigth)
-        return Rect((qrAndMetaArea.x - cellSide * 6).toInt(), (qrAndMetaArea.y + cellSide * markerCellHeigth).toInt(), (cellSide * 8).toInt(), (cellSide * 2).toInt())
+        return Rect(
+            (qrAndMetaArea.x - cellSide * 6).toInt(),
+            (qrAndMetaArea.y + cellSide * markerCellHeigth).toInt(),
+            (cellSide * 8).toInt(),
+            (cellSide * 2).toInt()
+        )
     }
 }
