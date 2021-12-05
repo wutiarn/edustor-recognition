4b1745e26c293a9bdd196575b8ac997e225b0528
Dmitry Romanov <me@wtrn.ru>
2021-12-05 19:18:56 +0300

    Update spire pdf

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,19 @@ plugins {
     id("org.springframework.boot") version "2.6.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.0"
+    kotlin("kapt") version "1.6.0"
     kotlin("plugin.spring") version "1.6.0"
 }
 
 group = "ru.wtrn.edustor"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
 
 repositories {
     mavenCentral()
@@ -29,21 +36,30 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.github.microutils:kotlin-logging-jvm:2.0.2")
     implementation("com.google.zxing:javase:3.4.1")
+    // SpirePDF has been downloaded to avoid using proprietary IceBlue repo
+    // Source: https://repo.e-iceblue.com/nexus/content/groups/public/e-iceblue/spire.pdf.free/4.4.1/
     implementation(
         group = "",
-        name = "spire-pdf-free-4.3.0"
-    ) // SpirePDF has been downloaded to avoid using proprietary IceBlue repo
+        name = "spire.pdf.free-4.4.1"
+    )
     implementation(group = "", name = "opencv-453")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.getByName<Jar>("jar") {
+    // Disable plain jar
+    // See https://stackoverflow.com/questions/67663728/spring-boot-2-5-0-generates-plain-jar-file-can-i-remove-it
+    enabled = false
 }
